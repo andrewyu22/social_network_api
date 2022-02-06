175d021edb02b898bd16f27482ecaf525fa463b6
Andrew Yu <andrewyu209@gmail.com>
2022-02-05 20:34:47 -0500

    Added users routers, getAll, getById, CreateUser

---
+++ controllers/user-controller.js
@@ -0,0 +1,41 @@
+const { User } = require('../models');
+
+const userController = {
+    // Get all Users
+    getAllUser(req, res) {
+        User.find({})
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({ _id: -1 })
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
+    // Get one User by id
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .select('-__v')
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
+    // Create a new Users
+    createUser({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.json(err))
+    }
+}
+
+module.exports = userController;
---
+++ routes/api/user-routes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const {
+    getAllUser,
+    getUserById,
+    createUser
+} = require('../../controllers/user-controller');
+
+// /api/user
+router
+    .route('/')
+    .get(getAllUser)
+    .post(createUser);
+
+// /api/user/:id
+router
+    .route('/:id')
+    .get(getUserById);
+
+module.exports = router
